bc05ce6dd44f00f024d864b20709ac8ecee59abd
Raick Santana <127554158+rsantana30@users.noreply.github.com>
2024-01-16 10:34:44 +0100

    add result

---
+++ app/src/main/java/com/example/projectlogin/splashActivity.java
@@ -0,0 +1,26 @@
+package com.example.projectlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+@SuppressLint("CustomSplashScreen")
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(splashActivity.this, FormLogin.class);
+            startActivity(intent);
+            finish();
+        },3000);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/projectlogin/user_screen.java
@@ -0,0 +1,73 @@
+package com.example.projectlogin;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+public class user_screen extends AppCompatActivity {
+
+    private TextView nomeUsuario,emailUsuario;
+    private Button deslogar;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String usuarioID;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_screen);
+        IniciarComponentes();
+
+        deslogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(user_screen.this,FormLogin.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                if (documentSnapshot != null){
+                    nomeUsuario.setText(documentSnapshot.getString("nome"));
+                    emailUsuario.setText(email);
+                }
+            }
+        });
+
+    }
+
+    private void IniciarComponentes(){
+
+        nomeUsuario = findViewById(R.id.text_usuario);
+        emailUsuario = findViewById(R.id.email_usuario);
+        deslogar = findViewById(R.id.deslogar);
+
+    }
+}
